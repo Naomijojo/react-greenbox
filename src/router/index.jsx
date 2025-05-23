cf637c1b2fb13b03ebaaf6d517993e3bec653cb8
JOJO <ch420ou@gmail.com>
2025-05-23 16:39:07 +0800

    init commit

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Home } from "@/pages/Home";
+import Layout from '@/components/Layout'
+import { Home, NotFound } from "@/pages";
 
 const router = createBrowserRouter([
   {
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"; 
 import tailwindcss from 'tailwindcss' 
-import path from "path";
 
 
 export default defineConfig({
@@ -17,4 +17,3 @@ export default defineConfig({
     },
   },
 })
-
